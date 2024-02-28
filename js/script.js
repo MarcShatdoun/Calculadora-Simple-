8520f9ad3a88285b844eca4ed93a90dd0b7b92f4
Marc Quimis <marc.quimis@gmail.com>
2024-02-28 13:25:11 +0100

    completado todo

--- js/script.js
+++ js/script.js
@@ -1,68 +1,80 @@
 function calcular(){
-    // limpiar las advertencias
-    let error1 = document.getElementById("error1")
-    error1.innerText = ""
-    let error2 = document.getElementById("error2")
-    error2.innerText = ""
-    let resultado = document.getElementById("resultado")
-    resultado = 0
-// Obtener los numeros
+        // limpiar las advertencias
+        let error1 = document.getElementById("error1")
+        error1.innerText = ""
+        let error2 = document.getElementById("error2")
+        error2.innerText = ""
+        let resultado = document.getElementById("resultado")
+    // resultado = 0
+    // Obtener los numeros
+        
+        let num1 = document.getElementById("num1").value;
+        let num2 = document.getElementById("num2").value; 
+
     
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value; 
 
- 
-
-// errores 
-    let errores = false;
-    num1 = num1.trim()
+    // errores 
+        let errores = false;
+        num1 = num1.trim()
 
 
 
-if(isNaN(num1) || num1 == "" ){
-    error1.innerText = "entrada inválida"
-    errores = true;
+    if(isNaN(num1) || num1 == "" ){
+        error1.innerText = "entrada inválida"
+        errores = true;
 
-}
+    }
 
-if (isNaN(num2) || num2 == "" ){
-    error2.innerText = "entrada inválida"
-    errores = true;
-}
+    if (isNaN(num2) || num2 == "" ){
+        error2.innerText = "entrada inválida"
+        errores = true;
+    }
 
-if (errores == true) {
-    return;
-}
-// console.log("num1 al principo es", typeof(num1))
-// console.log("num2 al principo es", typeof(num2))
+    if (errores == true) {
+        return;
+    }
+     console.log("num1 al principo es", typeof(num1))
+     console.log("num2 al principo es", typeof(num2))
 
-//Convertir en entero
-num1  = parseFloat(num1)
-num2 = parseFloat(num2)
+    //Convertir en entero
+    num1  = parseFloat(num1)
+    num2 = parseFloat(num2)
 
-console.log("num1 ahora es", typeof(num1))
-console.log("num2 ahora es", typeof(num2))
+    console.log("num1 ahora es", typeof(num1))
+    console.log("num2 ahora es", typeof(num2))
 
-let opciones = document.getElementsByName("operador")
+    let opciones = document.getElementsByName("operador")
 
-// console.log(opciones)
+    // console.log(opciones)
 
-let operacion = ""
+    let operacion = ""
 
-for(let i = 0; i < opciones.length; i++ ){
-    if(opciones[i].checked == true){
-    operacion = opciones[i].id;
-    break;
+    for(let i = 0; i < opciones.length; i++ ){
+        if(opciones[i].checked == true){
+        operacion = opciones[i].id;
+        break;
+        }
+    }
+
+    if (operacion == "sumar"){
+        resultado.innerText = num1 + num2
+    }   else if(operacion == "restar"){
+        resultado.innerText = num1 - num2
+    }   else if(operacion == "multiplicar"){
+        resultado.innerText = num1 * num2
+    }   else if(operacion == "dividir"){
+        if (num2 == 0){
+            resultado.innerText = "No se puede dividir por cero"
+        return;
+        }
+        resultado.innerText = num1 / num2
     }
 }
 
-if (operacion == "sumar"){
-    document.getElementById("resultado").innerText = num1 + num2
-}   else if(operacion == "restar"){
-    document.getElementById("resultado").innerText = num1 - num2
-}   else if(operacion == "multiplicar"){
-    document.getElementById("resultado").innerText = num1 * num2
-}   else if(operacion == "dividir"){
-    document.getElementById("resultado").innerText = num1 / num2
-}
+
+
+
+
+function reset(){
+    document.getElementsByClassName("resultado").innerText = 0
 }
